77f1f15b3eb8f3189c72d5c0c73cdec5cfb187da
Dragoon Artist <dragoon.artist@gmail.com>
2022-03-12 07:57:17 +0900

    Initializing Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.log("Hello White?");
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 5293;
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+app.get("/", (req, res) => res.send("Hello~, World"));
+
+const handleListening = () => {
+  console.log(`Lisening On http://localhost:${PORT}`);
+};
+app.listen(PORT, handleListening);
